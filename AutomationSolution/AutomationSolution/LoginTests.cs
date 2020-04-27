88edf0584bf2b96333ab6327eaf7614992ae6e68
Chirila <george.chirila@centric.eu>
2020-04-27 17:54:51 +0300

    Initial commit

---
+++ AutomationSolution/AutomationSolution/LoginTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationSolution
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Should_Login_Successfully()
+        {
+            var driver = new ChromeDriver();
+            try
+            {
+
+                driver.Manage().Window.Maximize();
+                driver.Navigate().GoToUrl("http://a.testaddressbook.com/");
+                driver.FindElement(By.Id("sign-in")).Click();
+                //BAD BAD BAD PRACTICE
+                Thread.Sleep(1000);
+                driver.FindElement(By.CssSelector("input[data-test='email']")).SendKeys("asd@asd.asd");
+                driver.FindElement(By.Name("session[password]")).SendKeys("asd");
+                driver.FindElement(By.CssSelector("input[value='Sign in']")).Click();
+
+                Assert.IsTrue(driver.FindElement(By.CssSelector("span[data-test='current-user']"))
+                    .Text.Equals("asd@asd.asd"));
+
+                Assert.AreEqual("asd@asd.asd",
+                    driver.FindElement(By.CssSelector("span[data-test='current-user']")).Text);
+
+                Assert.IsTrue(driver.FindElement(By.CssSelector("span[data-test='current-user']")).Displayed);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
---
+++ AutomationSolution/AutomationSolution/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationSolution")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationSolution")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("698b0d90-3daf-4728-80f6-464949b5ae4b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
